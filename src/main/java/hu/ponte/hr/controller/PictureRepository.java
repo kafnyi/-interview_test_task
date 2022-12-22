c46307e48b857288a86fdacc6687d901e3c22eda
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-12-22 21:26:48 +0100

    PictureRepository created

--- src/main/java/hu/ponte/hr/controller/PictureRepository.java
+++
@@ -1,8 +0,0 @@
-package hu.ponte.hr.controller;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PictureRepository extends JpaRepository<Picture,Long> {
-}
---
+++ src/main/java/hu/ponte/hr/services/PictureRepository.java
@@ -0,0 +1,9 @@
+package hu.ponte.hr.services;
+
+import hu.ponte.hr.model.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+}
