dc8ea81ca40d0594454f282cea24d85915664601
kafnyi <83401787+kafnyi@users.noreply.github.com>
2022-12-22 21:35:04 +0100

    ImagesController and UploadController endpoints implemented

--- src/main/java/hu/ponte/hr/controller/ImagesController.java
+++ src/main/java/hu/ponte/hr/controller/ImagesController.java
@@ -1,7 +1,10 @@
 package hu.ponte.hr.controller;
 
 
-import hu.ponte.hr.services.ImageStore;
+import hu.ponte.hr.model.ImageMeta;
+import hu.ponte.hr.services.ImageService;
+import hu.ponte.hr.services.PictureRepository;
+import hu.ponte.hr.services.SignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,23 +12,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
 import java.util.List;
 
 @RestController()
 @RequestMapping("api/images")
 public class ImagesController {
 
-    @Autowired
-    private ImageStore imageStore;
+	private final PictureRepository pictureRepository;
+	private final SignService signService;
+	private final ImageService imageService;
 
-    @GetMapping("meta")
-    public List<ImageMeta> listImages() {
-		return Collections.emptyList();
-    }
+	@Autowired
+	public ImagesController(PictureRepository pictureRepository, SignService signService, ImageService imageService) {
+		this.pictureRepository = pictureRepository;
+		this.signService = signService;
+		this.imageService = imageService;
+	}
 
-    @GetMapping("preview/{id}")
-    public void getImage(@PathVariable("id") String id, HttpServletResponse response) {
+	@GetMapping("meta")
+	public List<ImageMeta> listImages() {
+		return imageService.listAllImage();
+	}
+
+	@GetMapping("preview/{id}")
+	public ImageMeta getImage(@PathVariable("id") String id, HttpServletResponse response) {
+		return imageService.createImageMetaFromPicture(pictureRepository.findById(Long.parseLong(id)).get());
 	}
 
 }
